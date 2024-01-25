a5d8d6455b05e82d043b522963558af593f1c26f
alexhasan99 <mohammad.hasan19999@gmail.com>
2024-01-25 16:18:02 +0100

    initialize massages micro service

---
+++ Journal_Massages/src/main/java/com/kurdistan/Journal_Massages/JournalMassagesApplication.java
@@ -0,0 +1,13 @@
+package com.kurdistan.Journal_Massages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalMassagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalMassagesApplication.class, args);
+	}
+
+}
---
+++ Journal_Massages/src/test/java/com/kurdistan/Journal_Massages/JournalMassagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurdistan.Journal_Massages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalMassagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
